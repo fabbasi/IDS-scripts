1432a1cfe9c59364ae740761a5d873338cd17ab1
Fahim Abbasi <mailtofahim@gmail.com>
2011-12-16 19:12:07 +1300

    reverse exp scripts added

---
+++ get-500-rev-scores.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+## Trigger to calculate edit distances across all 10 datasets 
+## Invokes alldist-with-graph python script
+## Usage:
+## python alldist-with-graph.py /path/to/dataset iteration /path/to/result
+## python alldist-with-graph.py /home/fimz/Dev/datasets/500-dataset/$i $i /home/fimz/Dev/datasets/500-results
+##
+rm list500.txt
+for i in `seq 1 3`
+do
+	echo $i
+	python alldist-with-graph.py /home/fimz/Dev/datasets/500-dataset/reverse/$i $i /home/fimz/Dev/datasets/500-results/reverse
+	mv /home/fimz/Dev/scripts/output/* /home/fimz/Dev/datasets/500-results/reverse/$i/
+done
+#sh run500-dynamic.sh
